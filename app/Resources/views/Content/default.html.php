f190577fbc30070bc86a775d539fe4e1939fd40e
Nickolay Grekalo <64259058+Grekoff18@users.noreply.github.com>
2020-09-29 20:04:42 +0300

    config_base_template

--- app/Resources/views/Content/default.html.php
+++ app/Resources/views/Content/default.html.php
@@ -1,9 +1,10 @@
 <?php
+    // appending base template
     $this->extend('base.html.php');
+    // rewriting title tag
     $this->slots()->set('title', 'SignIn');
 ?>
-
-<h1><?=$this->input('form_headline')->render();?></h1>
+<!--Sign In Form-->
 <div id="form_wrapper">
     <form action="" id="signin">
         <input type="text" id="username" name="username" placeholder="Type your username">
--- app/Resources/views/base.html.php
+++ app/Resources/views/base.html.php
@@ -1,13 +1,23 @@
-<?php
-?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> <?php $this->slots()->output('title', 'Main Page'); ?> </title>
+    <?php
+        // append some meta tags dynamically
+        $this->headMeta()->appendName('viewport', 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0');
+        $this->headMeta()->appendHttpEquiv('X-UA-Compatible', 'ie=edge');
+        echo $this->headMeta();
+    ?>
+    <!-- this is dinamically title tag and you can rewrite it in other files -->
+    <title><?php $this->slots()->output('title', 'Main Page'); ?> </title>
+    <?php
+        // appending dinamically stylesheets
+        $this->headLink()->appendStylesheet('/var/assets/style.css');
+        $this->headLink()->prependStylesheet('/var/assets/dop.css', 'screen', true);
+        $this->headLink()->prependStylesheet('/var/assets/set.css', 'screen', true);
+        echo $this->headLink();
+    ?>
+
 </head>
 <body>
     <div class="wrapper">
